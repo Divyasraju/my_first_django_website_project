ab71d5136c33250e715b5fb85eba772ee9174a77
Divya <divyasraju93@gmail.com>
2018-08-18 11:57:55 +0530

    Word counter

--- my_first_django_website/urls.py
+++ my_first_django_website/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page),
-
+    path('count/', views.count_page),
 ]
--- my_first_django_website/views.py
+++ my_first_django_website/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse("<h1>Hello Divya<h1>")
+    return render(request, 'home.html')
+
+def count_page(request):
+    text = request.GET["usertext"]
+    char_len = len(text)
+    words_list = text.split()
+
+    word_dictionary = {}
+
+    for word in words_list:
+        if word in word_dictionary.keys():
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+
+    return render(request, 'count.html', {"text":text, "text_len":char_len, "words_len" : len(words_list), "word_dictionary": word_dictionary})
